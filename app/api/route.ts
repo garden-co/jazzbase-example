5b58a40cdb6621356704621e31aee3239e01bbfc
Anselm <anselm.eickhoff@gmail.com>
2025-07-29 16:33:18 -0700

    Also wait for chat creation

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,5 @@
 import { Chat, Message } from "../schema";
 import { co } from "jazz-tools";
-import { waitUntil } from "@vercel/functions";
 import { NextResponse } from "next/server";
 import { worker } from "../worker";
 
@@ -9,7 +8,9 @@ export async function POST() {
     messages: co.list(Message).create([]),
   });
 
-  waitUntil(worker.waitForAllCoValuesSync());
+  // wait for sync before returning to ensure consistency in
+  // fetches right afterwards
+  await worker.waitForAllCoValuesSync();
 
   return NextResponse.json({ chatId: chat.id }, { status: 200 });
 }
